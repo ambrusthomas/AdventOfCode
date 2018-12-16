9bc768b7a7e080ee47c0013710e7fdd0c304bbd3
Ambrus Tamás <ambrus.thomas@gmail.com>
2018-12-16 11:19:23 +0100

    day 12 done

--- day12.py
+++ day12.py
@@ -1,14 +1,13 @@
 raw_input = open("12.in", 'r').read()
 
-init = raw_input.splitlines()[0].split()[-1]
 rules = raw_input.splitlines()[2:]
 left = [k[:5] for k in rules]
 right = [k[-1] for k in rules]
 
 def part1():
-    global init
+    init = raw_input.splitlines()[0].split()[-1]
     leftmost = 0
-    for kk in range(20):
+    for _ in range(20):
         init = "...." + init + "...."
         s = ""
         for i in range(2, len(init) - 2):
@@ -18,7 +17,6 @@ def part1():
                 s += "."
         leftmost -= 2
         init = s
-
     sum = 0
     index = leftmost
     for c in init:
@@ -26,25 +24,28 @@ def part1():
         index += 1
     print(sum)
 
-def to_num(pot):
-    num = 0
-    for i in range(len(pot)):
-        if pot[i] == '#':
-            num += 2 ** i
-    return num
-
 def part2():
     init = raw_input.splitlines()[0].split()[-1]
-    init = "...." + init + "...."
-    nums = []
-    for i in range(2, len(init) - 2):
-        nums.append(to_num(init[i-2:i+3]))
-    print(nums)
-    leftmost = -2
-
-    for kk in range(20):
-        kk
-
+    leftmost = 0
+    turn = 0
+    while '##.####.####.....##.####.....##.####.....##.....##.####.####.####....##.####.####.####.....##.####.####.#.####.####......##.....####.####.####....##.####' not in init:
+        turn += 1
+        init = "...." + init + "...."
+        s = ""
+        for i in range(2, len(init) - 2):
+            if init[i - 2 : i + 3] in left:
+                s += right[left.index(init[i - 2 : i + 3])]
+            else:
+                s += "."
+        leftmost -= 2
+        init = s
+    sum = 0
+    index = leftmost
+    for c in init:
+        if c == '#': sum += index
+        index += 1
+    sum += init.count('#') * (50_000_000_000 - turn)
+    print(sum)
 
 part1()
 part2()
