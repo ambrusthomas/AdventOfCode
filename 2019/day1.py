fbad931ef06b3514bdae8d0ed7c8adb5ce83d203
Ambrus Tamas <ambrus.thomas@gmail.com>
2019-12-01 06:55:06 +0100

    day 1 done

---
+++ 2019/day1.py
@@ -0,0 +1,24 @@
+raw_input = open("1.in", 'r').read().splitlines()
+input = list(map(int, raw_input))
+
+def calcFuel(massOrFuel):
+	return max(0, massOrFuel // 3 - 2)
+
+def part1():
+    print(sum(map(calcFuel, input)))
+
+def recursiveCalcFuel(massOrFuel):
+	res = 0
+	requiredFuel = calcFuel(massOrFuel)
+
+	while requiredFuel:
+		res += requiredFuel
+		requiredFuel = calcFuel(requiredFuel)
+
+	return res
+
+def part2():
+    print(sum(map(recursiveCalcFuel, input)))
+
+part1()
+part2()
