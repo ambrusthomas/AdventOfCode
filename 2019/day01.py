9f14968c5a4f77b5d85de5060806db2eca9e0170
Ambrus Tamas <ambrus.thomas@gmail.com>
2019-12-02 07:10:32 +0100

    change tabs to spaces

--- 2019/day01.py
+++ 2019/day01.py
@@ -2,20 +2,20 @@ raw_input = open("01.in", 'r').read().splitlines()
 input = list(map(int, raw_input))
 
 def calcFuel(massOrFuel):
-	return max(0, massOrFuel // 3 - 2)
+    return max(0, massOrFuel // 3 - 2)
 
 def part1():
     print(sum(map(calcFuel, input)))
 
 def recursiveCalcFuel(massOrFuel):
-	res = 0
-	requiredFuel = calcFuel(massOrFuel)
+    res = 0
+    requiredFuel = calcFuel(massOrFuel)
 
-	while requiredFuel:
-		res += requiredFuel
-		requiredFuel = calcFuel(requiredFuel)
+    while requiredFuel:
+        res += requiredFuel
+        requiredFuel = calcFuel(requiredFuel)
 
-	return res
+    return res
 
 def part2():
     print(sum(map(recursiveCalcFuel, input)))
--- 2019/day02.py
+++ 2019/day02.py
@@ -4,35 +4,36 @@ input = list(map(int, raw_input))
 APOLLO_11_LANDING = 19690720
 
 def simulate(memory, noun, verb):
-	memory[1] = noun
-	memory[2] = verb
+    memory[1] = noun
+    memory[2] = verb
 
-	ip = 0
 
-	while ip < len(memory) and memory[ip] != 99:
-		opcode = memory[ip]
+    ip = 0
 
-		if opcode == 1:
-			memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]]
-		else:
-			memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]]
+    while ip < len(memory) and memory[ip] != 99:
+        opcode = memory[ip]
 
-		ip += 4
+        if opcode == 1:
+            memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]]
+        else:
+            memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]]
 
-	return memory[0]
+        ip += 4
+
+    return memory[0]
 
 def part1():
-	print(simulate(input.copy(), 12, 2))
+    print(simulate(input.copy(), 12, 2))
 
 def part2():
-	for noun in range(100):
-		for verb in range(100):
+    for noun in range(100):
+        for verb in range(100):
 
-			simulation_result = simulate(input.copy(), noun, verb)
+            simulation_result = simulate(input.copy(), noun, verb)
 
-			if simulation_result == APOLLO_11_LANDING:
-				print(100 * noun + verb)
-				return
+            if simulation_result == APOLLO_11_LANDING:
+                print(100 * noun + verb)
+                return
 
 part1()
 part2()
